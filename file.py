867727505d7f93229eedd884a36994171505e09c
hiepietel <jachumail@yahoo.pl>
2019-05-14 12:46:11 +0200

    window with server

---
+++ file.py
@@ -0,0 +1,83 @@
+import kivy
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.uix.pagelayout import PageLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+#from server import *
+from threading import Thread
+import socket
+
+class MyPageLayout(BoxLayout):
+    ip = "192.168.0.196"
+    # ip = 'localhost'
+    port = 6666
+    def __init__(self, **kwargs):
+        super(MyPageLayout,self).__init__(**kwargs)
+        self.serv = self.server()
+        print("server initialized")
+        #self.sock = MySocket()
+       #Thread(target=self.get_data).start()
+        Thread(target=self.serverStart).start()
+        self.add_widget(
+            Button(
+                text="Hello",
+                size_hint=(.5, .5)
+            )
+        )
+
+    def serverStart(self):
+            #self.serverRun()
+            #pass
+            while True:
+                self.serverRun()
+
+    def server(self):
+        proto = socket.getprotobyname('tcp')  # [1]
+        self.serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto)
+
+        self.serv.bind((self.ip, self.port))  # [2]
+        self.serv.listen(1)  # [3]
+        return self.serv
+
+    def serverRun(self):
+        conn, addr = self.serv.accept()
+        print("connected")
+        # conn.send(b'hello') # [4]
+        run = True;
+        while True:
+            message = conn.recv(64)
+            # print(str(message))                                      # [5]
+            if message:
+                print("message " + str(message))
+                #self.text = str(message)
+                conn.send(b'i received: ' + message)
+                if message == b'exit':
+                    print("exit")
+                    conn.close()
+                    run = False
+                    break;
+                if message == b'yes':
+                    print("yes")
+                    conn.send(b'say yes')
+                if message == b'say update':
+                    print("update")
+                    conn.send(b'update')
+                    # conn.close()
+            else:
+                break
+            run = False
+        conn.close()
+        run = False
+
+
+class BoxApp(App):
+
+    def build(self):
+        #pass
+        return MyPageLayout()
+
+
+#if __name__ == '__main__':
+BoxApp().run()
