7008230f767b71ba525e2de0b699a82c797e0ca2
hiepietel <jachumail@yahoo.pl>
2019-05-22 14:06:20 +0200

    almost done

--- file.py
+++ file.py
@@ -19,39 +19,54 @@ clock = str("00:00:00")
 
 class MyPageLayout(BoxLayout):
     ip = "192.168.0.196"
-    # ip = 'localhost'
+    #ip = 'localhost'
     port = 6666
     clock = StringProperty
+    alarmHour = -1
+    alarmMinute = -1
+    alarmed = False
 
     def __init__(self, **kwargs):
         super(MyPageLayout, self).__init__(**kwargs)
-
+        self.alarmed = False
         self.rect = Rectangle(size=self.size, pos=self.pos)
         self.serv = self.server()
-        self.orientation="vertical"
+        self.orientation = "vertical"
         self.color = (1, 0, 1, 1)
         #self.sock = MySocket()
        #Thread(target=self.get_data).start()
 
-        self.lbl = Label(
+        self.timelbl = Label(
                 text=clock,
                 bold=True,
-                color=(1,0,1,1),
-                font_size = "30sp",
+                color=(1, 0, 1, 1),
+                font_size="90sp",
                 size_hint=(1, .5),
                 id="clockLbl"
             )
-        self.lbl2 = Label(
-            text=clock,
-            bold=True,
-            color=(1, 0, 1, 1),
-            font_size="30sp",
-            size_hint=(1, .1),
-            id="clockLbll"
-        )
+
+        self.datelbl = Label(
+                text=clock,
+                bold=True,
+                color=(1, 0, 1, 1),
+                font_size="40sp",
+                size_hint=(1, .1),
+                id="clockLbll"
+            )
+
+        self.allarmlbl = Label(
+                text = "Alarm not set up",
+                bold = False,
+                font_size="20sp",
+                color=(0, 0, 0, 0),
+                size_hint=(1, .3)
+
+            )
+
         #self.(self.bgRect)
-        self.add_widget(self.lbl)
-        self.add_widget(self.lbl2)
+        self.add_widget(self.timelbl)
+        self.add_widget(self.datelbl)
+        self.add_widget(self.allarmlbl)
         Thread(target=self.serverStart).start()
         #Thread(1,target=self.time).start()
         Thread(target=self.time).start()
@@ -65,11 +80,12 @@ class MyPageLayout(BoxLayout):
         while True:
             #print("clock")
             myTime = time.gmtime()
+            myTime = time.localtime()
             myTimeStr = ""
             if myTime.tm_hour+2 < 10:
-                myTimeStr += "0"+str(myTime.tm_hour+2)+":"
+                myTimeStr += "0"+str(myTime.tm_hour)+":"
             else:
-                myTimeStr += str(myTime.tm_hour+2)+":"
+                myTimeStr += str(myTime.tm_hour)+":"
             if myTime.tm_min < 10:
                 myTimeStr += "0"+str(myTime.tm_min)+":"
             else:
@@ -89,10 +105,11 @@ class MyPageLayout(BoxLayout):
             else:
                 myDateStr += str(myTime.tm_mon) + "-"
             myDateStr += str(myTime.tm_year)
-
-
-            self.lbl.text = myTimeStr
-            self.lbl2.text = myDateStr
+            #check alarm
+            if self.alarmHour == myTime.tm_hour and self.alarmMinute == myTime.tm_min:
+                print("alarm")
+            self.timelbl.text = myTimeStr
+            self.datelbl.text = myDateStr
             ##print(myTimeStr)
             time.sleep(1)
 
@@ -125,20 +142,38 @@ class MyPageLayout(BoxLayout):
                     print("exit")
                     conn.close()
                     run = False
-                    break;
-                if message == b'yes':
-                    print("yes")
-                    conn.send(b'say yes')
+                    break
+                if message[0] == 115 and message[1] == 97:
+                    self.alarmHour = int(message[2]-48)*10 + int(message[3]-48)
+                    self.alarmMinute = int(message[5]-48)*10 + int(message[6]-48)
+                    print("alarm set to: "+str(self.alarmHour)+" : "+str(self.alarmMinute))
+                    self.allarmlbl.text = "Alarm: "+str(int(message[2]-48)) + str(int(message[3]-48)) + " : " + str(int(message[5]-48)) + str(int(message[6]-48))
+                if message == b'alarmOn':
+                    self.allarmlbl.color = self.timelbl.color
+                    print("alarm on")
+                    self.alarmed = True
+                if message == b'alarmOff':
+                    self.allarmlbl.color = (0, 0, 0, 0)
+                    print("alarm off")
+                    self.alarmed = False
                 if message == b'purple':
                     print("font color is purple")
-                    self.lbl.color = (1, 0, 1, 1)
-                if message == b'white':
-                    print("font color is white")
-                    self.lbl.color = (1, 1, 1, 1)
+                    self.timelbl.color = (1, 0, 1, 1)
+                    self.datelbl.color = (1, 0, 1, 1)
+                    if self.alarmed:
+                        self.allarmlbl.color = (1, 0, 1, 1)
+                if message == b'green':
+                    print("font color is green")
+                    self.timelbl.color = (0, 1, 0, 1)
+                    self.datelbl.color = (0, 1, 0, 1)
+                    if self.alarmed:
+                        self.allarmlbl.color = (0, 1, 0, 1)
                 if message == b'cyan':
                     print("font color is cyan")
-                    self.lbl.color = (0, 1, 1, 1)
-
+                    self.timelbl.color = (0, 1, 1, 1)
+                    self.datelbl.color = (0, 1, 1, 1)
+                    if self.alarmed:
+                        self.allarmlbl.color = (0, 1, 1, 1)
                 if message == b'say update':
                     print("update")
                     conn.send(b'update')
